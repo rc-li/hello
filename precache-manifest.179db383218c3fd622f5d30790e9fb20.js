cefe66369310ef6c9b50aaa213739e78556ee994
rc-li <ruicheng.li@uconn.edu>
2020-02-19 19:08:44 -0800

    Updates

---
+++ precache-manifest.179db383218c3fd622f5d30790e9fb20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6daf650cf7cb5fef2c55738d373f62",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "a6d1757e10b379cc665f",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95b661d4d4129b38619a",
+    "url": "/hello/static/js/2.6949f162.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello/static/js/2.6949f162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d1757e10b379cc665f",
+    "url": "/hello/static/js/main.01bea9c4.chunk.js"
+  },
+  {
+    "revision": "de368dcbb9a951cc3091",
+    "url": "/hello/static/js/runtime-main.033cf731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
